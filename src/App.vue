67fa7ffaa337651ac6e1150b07676570bd07d2e4
IT5706021610118 <tonokhot@hotmail.co.th>
2017-02-27 00:53:28 +0700

    after css

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,79 @@
 <template>
-  <div class="hello">
+  <div  class="card container" >
 
-      Keywords : <input type="text" name="" v-model="keyword">
-      <button v-on:click="preProcessing" type="submit" class="btn btn-default">OK</button>
-      <h1>{{Key_adjust}}</h1>
+      <div class="w3-padding-16 w3-center">
+        <b id="x1">Playfair Cipher Algorithm</b>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Key </label>
+              <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Key" v-model="keyword">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Plain Text </label>
+              <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Plaintext" v-model="plaintext_new">
+            </div>
+            <div class="w3-center">
+              <button type="submit" class="btn btn-default" v-on:click="preProcessing"><b>OK</b></button>
+            </div>
+          </form>
+          <div class="w3-padding-32">
+            <p><b>Plain  Text : </b>{{plaintext_show}} </p>
+            <p><b>Cipher Text : </b>{{output}} </p>
+          </div>
 
-      Plaintext : <input type="text" name="" v-model="plaintext_new">
-      <button v-on:click="prePlaintext" type="submit" class="btn btn-default">OK</button>
-      <h1>{{plainDivided_adjust}}</h1>
-    <table border="2" v-if="Key_adjust">
-      <tr>
-        <td :bgcolor="color[0][0]">{{Key_2D[0][0]}}</td>
-        <td :bgcolor="color[0][1]">{{Key_2D[0][1]}}</td>
-        <td :bgcolor="color[0][2]">{{Key_2D[0][2]}}</td>
-        <td :bgcolor="color[0][3]">{{Key_2D[0][3]}}</td>
-        <td :bgcolor="color[0][4]">{{Key_2D[0][4]}}</td>
-      </tr>
-      <tr>
-        <td :bgcolor="color[1][0]">{{Key_2D[1][0]}}</td>
-        <td :bgcolor="color[1][1]">{{Key_2D[1][1]}}</td>
-        <td :bgcolor="color[1][2]">{{Key_2D[1][2]}}</td>
-        <td :bgcolor="color[1][3]">{{Key_2D[1][3]}}</td>
-        <td :bgcolor="color[1][4]">{{Key_2D[1][4]}}</td>
-      </tr>
-      <tr>
-        <td :bgcolor="color[2][0]">{{Key_2D[2][0]}}</td>
-        <td :bgcolor="color[2][1]">{{Key_2D[2][1]}}</td>
-        <td :bgcolor="color[2][2]">{{Key_2D[2][2]}}</td>
-        <td :bgcolor="color[2][3]">{{Key_2D[2][3]}}</td>
-        <td :bgcolor="color[2][4]">{{Key_2D[2][4]}}</td>
-      </tr>
-      <tr>
-        <td :bgcolor="color[3][0]">{{Key_2D[3][0]}}</td>
-        <td :bgcolor="color[3][1]">{{Key_2D[3][1]}}</td>
-        <td :bgcolor="color[3][2]">{{Key_2D[3][2]}}</td>
-        <td :bgcolor="color[3][3]">{{Key_2D[3][3]}}</td>
-        <td :bgcolor="color[3][4]">{{Key_2D[3][4]}}</td>
-      </tr>
-      <tr>
-        <td :bgcolor="color[4][0]">{{Key_2D[4][0]}}</td>
-        <td :bgcolor="color[4][1]">{{Key_2D[4][1]}}</td>
-        <td :bgcolor="color[4][2]">{{Key_2D[4][2]}}</td>
-        <td :bgcolor="color[4][3]">{{Key_2D[4][3]}}</td>
-        <td :bgcolor="color[4][4]">{{Key_2D[4][4]}}</td>
-      </tr>
-    </table><br />
-    <button @click="encrypt(plainDivided_adjust)" v-show="letEn">Encrypt</button>
-    <h1>Output : {{output}}</h1>
+        </div>
+        <div class="col-md-6">
+      <div class="w3-padding-8">
+        <table border="2" v-if="Key_adjust" align=center>
+          <tr class="w3-center">
+            <td :bgcolor="color[0][0]">{{Key_2D[0][0]}}</td>
+            <td :bgcolor="color[0][1]">{{Key_2D[0][1]}}</td>
+            <td :bgcolor="color[0][2]">{{Key_2D[0][2]}}</td>
+            <td :bgcolor="color[0][3]">{{Key_2D[0][3]}}</td>
+            <td :bgcolor="color[0][4]">{{Key_2D[0][4]}}</td>
+          </tr>
+          <tr class="w3-center">
+            <td :bgcolor="color[1][0]">{{Key_2D[1][0]}}</td>
+            <td :bgcolor="color[1][1]">{{Key_2D[1][1]}}</td>
+            <td :bgcolor="color[1][2]">{{Key_2D[1][2]}}</td>
+            <td :bgcolor="color[1][3]">{{Key_2D[1][3]}}</td>
+            <td :bgcolor="color[1][4]">{{Key_2D[1][4]}}</td>
+          </tr>
+          <tr class="w3-center">
+            <td :bgcolor="color[2][0]">{{Key_2D[2][0]}}</td>
+            <td :bgcolor="color[2][1]">{{Key_2D[2][1]}}</td>
+            <td :bgcolor="color[2][2]">{{Key_2D[2][2]}}</td>
+            <td :bgcolor="color[2][3]">{{Key_2D[2][3]}}</td>
+            <td :bgcolor="color[2][4]">{{Key_2D[2][4]}}</td>
+          </tr>
+          <tr class="w3-center">
+            <td :bgcolor="color[3][0]">{{Key_2D[3][0]}}</td>
+            <td :bgcolor="color[3][1]">{{Key_2D[3][1]}}</td>
+            <td :bgcolor="color[3][2]">{{Key_2D[3][2]}}</td>
+            <td :bgcolor="color[3][3]">{{Key_2D[3][3]}}</td>
+            <td :bgcolor="color[3][4]">{{Key_2D[3][4]}}</td>
+          </tr>
+          <tr class="w3-center">
+            <td :bgcolor="color[4][0]">{{Key_2D[4][0]}}</td>
+            <td :bgcolor="color[4][1]">{{Key_2D[4][1]}}</td>
+            <td :bgcolor="color[4][2]">{{Key_2D[4][2]}}</td>
+            <td :bgcolor="color[4][3]">{{Key_2D[4][3]}}</td>
+            <td :bgcolor="color[4][4]">{{Key_2D[4][4]}}</td>
+          </tr>
+        </table><br />
+      </div>
+      </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="w3-padding-4 w3-center" v-if="Key_adjust">
+            <center><button @click="encrypt(plainDivided_adjust)" v-show="letEn" class="btn btn-default"><b>Encrypt</b></button></center>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
@@ -76,6 +103,7 @@ export default {
       plainDivided: [],
       plainDivided_adjust: [],
       plaintext_new: '',
+      plaintext_show: '',
       output: '',
       count: 0,
       letEn: true,
@@ -101,6 +129,7 @@ export default {
       vm.setKey()
       vm.genKey()
       vm.format()
+      vm.prePlaintext()
     },
     setKey () {
       let vm = this
@@ -203,6 +232,9 @@ export default {
       } else {
         vm.plainDivided_adjust[vm.plainDivided_adjust.length - 1] = vm.plainDivided_adjust[vm.plainDivided_adjust.length - 1] + 'Z'
       }
+      for (var n = 0; n < vm.plainDivided_adjust.length; n++) {
+        vm.plaintext_show += vm.plainDivided_adjust[n] + ' '
+      }
     },
     encrypt (item) {
       let vm = this
@@ -231,21 +263,21 @@ export default {
               if (a === 0) {
                 position.char1.row = r
                 position.char1.column = c
-                vm.color[r][c] = 'green'
+                vm.color[r][c] = '#33FF80'
               } else {
                 position.char2.row = r
                 position.char2.column = c
-                vm.color[r][c] = 'green'
+                vm.color[r][c] = '#33FF80'
               }
             } else if (check[a] === 'J' && vm.Key_2D[r][c] === 'I') {
               if (a === 0) {
                 position.char1.row = r
                 position.char1.column = c
-                vm.color[r][c] = 'green'
+                vm.color[r][c] = '#33FF80'
               } else {
                 position.char2.row = r
                 position.char2.column = c
-                vm.color[r][c] = 'green'
+                vm.color[r][c] = '#33FF80'
               }
             }
           }
@@ -256,30 +288,30 @@ export default {
           if (position.char1.column !== position.char2.column) {
             if (j === 0) {
               check[j] = vm.Key_2D[position.char1.row][position.char2.column]
-              vm.color[position.char1.row][position.char2.column] = 'red'
+              vm.color[position.char1.row][position.char2.column] = '#FF3333'
             } else {
               check[j] = vm.Key_2D[position.char2.row][position.char1.column]
-              vm.color[position.char2.row][position.char1.column] = 'red'
+              vm.color[position.char2.row][position.char1.column] = '#FF3333'
             }
             vm.output += check[j]
           } else {
             if (j === 0) {
               if (position.char1.row !== 4) {
                 check[j] = vm.Key_2D[position.char1.row + 1][position.char1.column]
-                vm.color[position.char1.row + 1][position.char1.column] = 'red'
+                vm.color[position.char1.row + 1][position.char1.column] = '#FF3333'
               } else {
                 position.char1.row = 0
                 check[j] = vm.Key_2D[position.char1.row][position.char1.column]
-                vm.color[position.char1.row][position.char1.column] = 'red'
+                vm.color[position.char1.row][position.char1.column] = '#FF3333'
               }
             } else {
               if (position.char2.row !== 4) {
                 check[j] = vm.Key_2D[position.char2.row + 1][position.char2.column]
-                vm.color[position.char2.row + 1][position.char2.column] = 'red'
+                vm.color[position.char2.row + 1][position.char2.column] = '#FF3333'
               } else {
                 position.char2.row = 0
                 check[j] = vm.Key_2D[position.char2.row][position.char2.column]
-                vm.color[position.char2.row][position.char2.column] = 'red'
+                vm.color[position.char2.row][position.char2.column] = '#FF3333'
               }
             }
             vm.output += check[j]
@@ -288,25 +320,26 @@ export default {
           if (j === 0) {
             if (position.char1.column !== 4) {
               check[j] = vm.Key_2D[position.char1.row][position.char1.column + 1]
-              vm.color[position.char1.row][position.char1.column + 1] = 'red'
+              vm.color[position.char1.row][position.char1.column + 1] = '#ff1a1a'
             } else {
               position.char1.column = 0
               check[j] = vm.Key_2D[position.char1.row][position.char1.column]
-              vm.color[position.char1.row][position.char1.column] = 'red'
+              vm.color[position.char1.row][position.char1.column] = '#ff1a1a'
             }
           } else {
             if (position.char2.column !== 4) {
               check[j] = vm.Key_2D[position.char2.row][position.char2.column + 1]
-              vm.color[position.char2.row][position.char2.column + 1] = 'red'
+              vm.color[position.char2.row][position.char2.column + 1] = '#ff1a1a'
             } else {
               position.char2.column = 0
               check[j] = vm.Key_2D[position.char2.row][position.char2.column]
-              vm.color[position.char2.row][position.char2.column] = 'red'
+              vm.color[position.char2.row][position.char2.column] = '#ff1a1a'
             }
           }
           vm.output += check[j]
         }
       }
+      vm.output += ' '
       vm.count++
       if (vm.count === item.length) {
         vm.letEn = false
@@ -317,11 +350,46 @@ export default {
 </script>
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+
+  font-family: 'Muli', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background-color: #BDBDBD;
   margin-top: 60px;
+
+}
+.card {
+  @import url('https://fonts.googleapis.com/css?family=Quicksand');
+  font-family: 'Quicksand', sans-serif;
+    /* Add shadows to create the "card" effect */
+    font-size: 20px;
+    background-color: white;
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    border-radius: 15px;
+
+}
+.container {
+    padding: 2px 16px;
+}
+table {
+  table-layout: auto;
+
+  border-collapse: collapse;
+  border: 1px solid black;
+  width: 100%;
+  height: 500px;
+
+
+}
+td {
+  text-align: center;
+  width: 20%;
+  height: 100px;
+}
+#x1 {
+
+  font-size:30px;
 }
 </style>
